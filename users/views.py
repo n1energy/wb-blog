d730d4df2963bc376a39856b22676d17fde5c2d5
dolphin <dosometh@mail.com>
2023-03-15 13:15:15 +1000

    init commit

---
+++ users/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from blog.users.serializers import UserSerializer
+from django.contrib.auth.models import User
+
+
+class UsersAPIView(APIView):
+    def get(self, request):
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
